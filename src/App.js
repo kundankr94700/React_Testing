4c8ed5c5bd21a2bbddb4566181ca842c37203b38
Kundan Kumar <kundankr94700@gmail.com>
2021-08-13 15:26:18 +0530

    todo Testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Todo from './components/todo'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Testing </h1>
+        <Todo/>
       </header>
     </div>
   );
---
+++ src/components/todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todo() {
+    return (
+        <div>
+            <h2>Tesing being done</h2>
+        </div>
+    )
+}
+
+export default Todo
